13ff438ce1359aa7d01b35030fb8e3ebd82029ce
s1f102201391 <s1f102201391@iniad.org>
2024-09-14 21:01:11 +0900

    merge

--- server/common/constants.ts
+++ server/common/constants.ts
@@ -1,10 +1,6 @@
 export const APP_NAME = 'CATAPULT';
 
-<<<<<<< HEAD
-export const ID_NAME_LIST = ['user', 'task'] as const;
-=======
 export const ID_NAME_LIST = ['user', 'task', 'room'] as const;
->>>>>>> e601fd90e2004678d8452d899b67729cfe7498c0
 
 export const IS_PROD = process.env.NODE_ENV === 'production';
 
