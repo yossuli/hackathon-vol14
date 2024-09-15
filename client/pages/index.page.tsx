0a4bfe265e208932b3f80fe3523369f51e4b58f7
Sho0226 <s1F102201616@iniad.org>
2024-09-15 14:08:03 +0900

    ホームページ作成

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,18 +1,31 @@
-import { TaskList } from 'features/tasks/TaskList';
-import { Layout } from 'layouts/Layout';
+import Header from './Header';
 import styles from './index.module.css';
 
-const YourComponent = () => {
+const Home = () => {
   return (
-    <Layout
-      render={(user) => (
-        <div className={styles.container}>
-          <div className={styles.title}>Hello {user.signInName}!</div>
-          <TaskList />
+    <div>
+      <Header />
+      <div className={styles.logo}>
+        <img src="/images/logo_navy.png" alt="Logo" className={styles.logoImage} />
+      </div>
+      <div className={styles.container}>
+        <a href="/hanabi_creator" className={styles.button}>
+          花火を作成する
+        </a>
+        <a href="/random" className={styles.button}>
+          ランダムに入室
+        </a>
+        <div className={styles.options}>
+          <a href="/penetrate" className={styles.optionButton}>
+            プライベートルーム
+          </a>
+          <a href="/works" className={styles.optionButton}>
+            作品
+          </a>
         </div>
-      )}
-    />
+      </div>
+    </div>
   );
 };
 
-export default YourComponent;
+export default Home;
