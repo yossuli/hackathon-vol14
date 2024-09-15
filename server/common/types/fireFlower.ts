2f1c9cb98e86846721b740a89158d76c1e1e905c
yossuli <s1f102302348@iniad.org>
2024-09-15 11:41:33 +0900

    add: type about fireFlower

---
+++ server/common/types/fireFlower.ts
@@ -0,0 +1,33 @@
+import type { z } from 'zod';
+import type { fireFlowerValidator } from '../validators/fireFlower';
+import type { DtoId } from './brandedId';
+
+export type FireFlower = z.infer<typeof fireFlowerValidator>;
+
+export type FireFlowerDto = {
+  id: DtoId['fireFlower'];
+  name: string;
+  createdAt: number;
+  updatedAt: number;
+  structure: FireFlower;
+  creator: { id: DtoId['user']; signInName: string };
+};
+
+export type FireFlowerCreateVal = {
+  name: string;
+  structure: FireFlower;
+  creatorId: DtoId['user'];
+};
+
+export type FireFlowerUpdateVal = {
+  name?: string;
+  structure?: FireFlower;
+};
+
+export type FireFlowerFindVal = {
+  id?: DtoId['fireFlower'];
+  name?: string;
+  createdAt?: number;
+  updatedAt?: number;
+  creatorId?: DtoId['user'];
+};
