d347c79dd0451f0d15f31dabd4ddfe51b29bad27
yossuli <s1f102302348@iniad.org>
2024-12-11 03:11:30 +0900

    improve: use strict omit

--- server/domain/rooms/model/roomType.ts
+++ server/domain/rooms/model/roomType.ts
@@ -2,9 +2,9 @@ import type { RoomStatus } from '@prisma/client';
 import type { RoomDto } from 'common/types/room';
 import type { EntityId } from 'service/brandedId';
 
-export type RoomEntity = Omit<RoomDto, 'id' | 'creator'> & {
+export type RoomEntity = StrictOmit<RoomDto, 'id' | 'creator'> & {
   id: EntityId['room'];
-  creator: Omit<RoomDto['creator'], 'id'> & { id: EntityId['user'] };
+  creator: StrictOmit<RoomDto['creator'], 'id'> & { id: EntityId['user'] };
 } & RoomCreateServerVal;
 
 export type RoomCreateServerVal = { name: string; password?: string; status: RoomStatus };
