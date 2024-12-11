a628bec5c9b9206d145c3b72a5bb51d570c6de29
yossuli <s1f102302348@iniad.org>
2024-12-12 00:30:04 +0900

    chore: add comment

--- server/tests/api/private/chat.test.ts
+++ server/tests/api/private/chat.test.ts
@@ -2,6 +2,7 @@ import { expect, test } from 'vitest';
 import { createSessionClients, noCookieClient } from '../apiClient';
 import { GET, POST } from '../utils';
 
+// チャットの投稿テスト
 test(POST(noCookieClient.private.chat._roomId('roomId')), async () => {
   const apiClient = await createSessionClients();
   const room = await apiClient.private.rooms.$post({
@@ -18,6 +19,7 @@ test(POST(noCookieClient.private.chat._roomId('roomId')), async () => {
   expect(res.status).toEqual(201);
 });
 
+// チャットの取得テスト
 test(GET(noCookieClient.private.chat), async () => {
   const apiClient = await createSessionClients();
   const res = await apiClient.private.chat.get();
@@ -26,8 +28,10 @@ test(GET(noCookieClient.private.chat), async () => {
   expect(res.body.length).toEqual(0);
 });
 
+// チャットの投稿と取得複合テスト
 test(GET(noCookieClient.private.chat), async () => {
   const apiClient = await createSessionClients();
+  // ルームの作成
   const room = await apiClient.private.rooms.$post({
     body: {
       name: 'test-room',
@@ -35,10 +39,11 @@ test(GET(noCookieClient.private.chat), async () => {
       status: 'PRIVATE',
     },
   });
+  // チャットの投稿
   const chat = await apiClient.private.chat._roomId(room.id).post({
     body: { content: 'test' },
   });
-
+  // チャットの取得
   const res = await apiClient.private.chat.get();
 
   expect(res.status).toEqual(200);
