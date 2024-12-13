fb897f0bdf6f45deb40d290fdc17bdd7aa400b61
s-renren <s1f102301156@iniad.org>
2024-12-14 00:28:24 +0900

    保存した花火を表示できるように

--- client/pages/hanabi_list/index.page.tsx
+++ client/pages/hanabi_list/index.page.tsx
@@ -1,33 +1,23 @@
-import { colors } from 'utils/colors/colors';
+import type { FireFlowerDto } from 'common/types/fireFlower';
+import { useEffect, useState } from 'react';
+import { apiClient } from 'utils/apiClient';
 import Header from '../../components/header/Header';
 import styles from './index.module.css';
 
 const works = () => {
-  const dummyData = {
-    flower1: {
-      flower: [
-        [0, 1, 2, 3, 4, 5, 6],
-        [23, 24, 25, 26, 27, 6, 7],
-        [22, 38, 39, 40, 41, 28, 8],
-        [21, 37, 46, 47, 42, 29, 9],
-        [20, 36, 45, 44, 43, 30, 10],
-        [19, 35, 34, 33, 32, 31, 11],
-        [18, 17, 16, 15, 14, 13, 12],
-      ],
-      name: 'Fire Flower',
-    },
-    flower2: {
-      flower: [
-        [60, 60, 60, 60, 60, 60, 60],
-        [60, 0, 0, 0, 0, 0, 60],
-        [60, 0, 25, 25, 25, 0, 60],
-        [60, 0, 25, 20, 25, 0, 60],
-        [60, 0, 25, 25, 25, 0, 60],
-        [60, 0, 0, 0, 0, 0, 60],
-        [60, 60, 60, 60, 60, 60, 60],
-      ],
-      name: 'Fire Flower2',
-    },
+  const [fireFlowerData, setFireFlowerData] = useState<FireFlowerDto[]>([]);
+  useEffect(() => {
+    fetchFireFlowerData();
+  }, []);
+
+  const fetchFireFlowerData = async () => {
+    try {
+      const res = await apiClient.private.fireFlowers.$get();
+      setFireFlowerData(res);
+      console.log('取得したデータ:', res);
+    } catch (error) {
+      console.error('データの取得中にエラーが発生しました:', error);
+    }
   };
 
   return (
@@ -36,18 +26,18 @@ const works = () => {
       <div className={styles.container}>
         <h1 className={styles.h1}>一覧</h1>
         <div className={styles.summary}>
-          {Object.entries(dummyData).map(([key, data]) => (
+          {Object.entries(fireFlowerData).map(([key, data]) => (
             <div key={key} className={styles.flowerBlock}>
               <div className={styles.fireFlower}>
                 <div className={styles.background}>
-                  {data.flower.map((row, i) => (
+                  {data.structure.map((row, i) => (
                     <div key={i} className={styles.pixel}>
                       {row.map((col, j) => (
                         <div
                           key={j}
                           className={styles.cell}
                           style={{
-                            backgroundColor: col === 0 ? 'white' : colors[col - 1],
+                            backgroundColor: col ?? 'transparent',
                           }}
                         ></div>
                       ))}
