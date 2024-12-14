224f516e56d3cde1bfc51b3492fba91776735ec8
yossuli <s1f102302348@iniad.org>
2024-12-14 14:48:58 +0900

    bihanai

--- server/service/websocket.ts
+++ server/service/websocket.ts
@@ -1,9 +1,16 @@
+import type { FireFlowerEntity } from 'domain/fireFlower/model/fireFlowerType';
+import { fireFlowerQuery } from 'domain/fireFlower/repository/fireFlowerQuery';
+import { roomQuery } from 'domain/rooms/repository/roomQuery';
 import type { FastifyPluginAsync } from 'fastify';
 import { WebSocket } from 'ws';
+import { brandedId } from './brandedId';
+import { prismaClient } from './prismaClient';
 
 const websocketHandler: FastifyPluginAsync = async (fastify) => {
   // すべてのクライアントを追跡
   const clients = new Set<WebSocket>();
+  const room = await roomQuery.listByCreatedAt(prismaClient).then((rooms) => rooms[0]);
+  let fireFlowers: FireFlowerEntity[];
 
   fastify.get('/ws', { websocket: true }, (socket) => {
     // クライアントをセットに追加
@@ -15,9 +22,51 @@ const websocketHandler: FastifyPluginAsync = async (fastify) => {
       console.log('Message received:', message.toString());
       for (const client of clients) {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(message.toString());
+          const returnMessage = JSON.stringify({
+            message: {
+              type: 'message',
+              message: message.toString(),
+            },
+          });
+          client.send(returnMessage);
         }
       }
+    });
+    // 定期的に花火を更新
+    let time2 = 0;
+    time2 = Math.floor(Math.random() * 10000) + 10000;
+    const interval2 = setInterval(async () => {
+      console.log('room?.id', room?.id);
+      fireFlowers = await fireFlowerQuery.findByRoomId(
+        prismaClient,
+        brandedId.room.dto.parse(room?.id),
+      );
+    }, time2);
+
+    // 定期的に花火をブロードキャスト
+    let time = 0;
+    time = Math.floor(Math.random() * 4000) + 1000;
+    const interval = setInterval(() => {
+      console.log('fireFlowers', fireFlowers);
+      if (!fireFlowers) return;
+      const message = JSON.stringify({
+        message: {
+          type: 'fireFlower',
+          fireFlower: fireFlowers[Math.floor(Math.random() * fireFlowers.length)],
+          x: Math.floor(Math.random() * 100),
+        },
+      });
+      for (const client of clients) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      }
+    }, time);
+
+    // ソケットが閉じられたときにインターバルをクリア
+    socket.on('close', () => {
+      clearInterval(interval);
+      clearInterval(interval2);
     });
 
     // クライアント切断時
